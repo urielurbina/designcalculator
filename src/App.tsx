36dda4f893a34a80a1a33e737666669e9e60b76a
urielurbina <soyurielurbina@gmail.com>
2024-12-12 00:39:20 -0600

    ocultar navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { SubscriptionProvider } from './contexts/SubscriptionContext';
 import { Toaster } from 'react-hot-toast';
@@ -19,41 +19,51 @@ import Pricing from './pages/Pricing';
 import AuthCallback from './components/AuthCallback';
 import ProtectedRoute from './components/ProtectedRoute';
 
+// Creamos un componente wrapper para acceder a useLocation
+const AppContent = () => {
+  const location = useLocation();
+  const isQuotePage = location.pathname === '/cotizar';
+
+  return (
+    <AuthProvider>
+      <SubscriptionProvider>
+        <Toaster position="top-right" />
+        <div className="flex flex-col min-h-screen">
+          {!isQuotePage && <PromoBanner />}
+          {!isQuotePage && <Navbar />}
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/calculator" element={<PriceCalculator />} />
+              <Route path="/freelance" element={<FreelanceCalculator />} />
+              <Route path="/colors" element={<ColorConverter />} />
+              <Route path="/privacy" element={<PrivacyPolicy />} />
+              <Route path="/terms" element={<TermsOfService />} />
+              <Route path="/diagnostic" element={<DiagnosticPage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/pricing" element={<Pricing />} />
+              <Route path="/auth/callback" element={<AuthCallback />} />
+              <Route 
+                path="/cotizar" 
+                element={
+                  <ProtectedRoute>
+                    <Cotizar />
+                  </ProtectedRoute>
+                } 
+              />
+            </Routes>
+          </main>
+          {!isQuotePage && <Footer />}
+        </div>
+      </SubscriptionProvider>
+    </AuthProvider>
+  );
+};
+
 function App() {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <SubscriptionProvider>
-          <Toaster position="top-right" />
-          <div className="flex flex-col min-h-screen">
-            <PromoBanner />
-            <Navbar />
-            <main className="flex-grow">
-              <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/calculator" element={<PriceCalculator />} />
-                <Route path="/freelance" element={<FreelanceCalculator />} />
-                <Route path="/colors" element={<ColorConverter />} />
-                <Route path="/privacy" element={<PrivacyPolicy />} />
-                <Route path="/terms" element={<TermsOfService />} />
-                <Route path="/diagnostic" element={<DiagnosticPage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/pricing" element={<Pricing />} />
-                <Route path="/auth/callback" element={<AuthCallback />} />
-                <Route 
-                  path="/cotizar" 
-                  element={
-                    <ProtectedRoute>
-                      <Cotizar />
-                    </ProtectedRoute>
-                  } 
-                />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-        </SubscriptionProvider>
-      </AuthProvider>
+      <AppContent />
     </BrowserRouter>
   );
 }
